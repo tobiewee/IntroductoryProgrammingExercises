93d8773942f7c3830ac241d3f0653f14924c35fa
Tobie Wee <tobiewee@gmail.com>
2016-06-04 11:04:24 +0800

    Draw a Horizontal Line

---
+++ src/DrawAHorizontalLine.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by Tobie Wee on 6/4/2016.
+ */
+public class DrawAHorizontalLine {
+    public static void main (String [] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.printf("Enter number of '*': ");
+        int n = in.nextInt();
+
+        for (int i=0; i<n; i++){
+            System.out.print('*');
+        }
+    }
+}
