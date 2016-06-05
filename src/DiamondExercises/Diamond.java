78ef9a1cafbeb518d9125fa9b27480bf0b167c7e
Tobie Wee <tobiewee@gmail.com>
2016-06-05 17:46:56 +0800

    Refactor Diamond codes

--- src/DiamondExercises/Diamond.java
+++ src/DiamondExercises/Diamond.java
@@ -7,6 +7,15 @@ import java.util.Scanner;
  * on 6/5/2016.
  */
 public class Diamond {
+    public static void drawLine(int width, int startPoint, int endPoint){
+        for (int j=0; j<width; j++) {
+            if (j >= startPoint && j <= endPoint)
+                System.out.print('*');
+            else
+                System.out.print(' ');
+        }
+    }
+
     public static void main (String [] args){
         Scanner in = new Scanner(System.in);
 
@@ -16,23 +25,15 @@ public class Diamond {
         int height = n+(n-1);
         int width = (n-1)+n;
         for (int i=0; i<height; i++) {
-            for (int j=0; j<width; j++) {
-                if(i<n) {
-                    int startDraw = (width / 2) - i;
-                    int endDraw = (width / 2) + i;
-                    if (j >= startDraw && j <= endDraw)
-                        System.out.print('*');
-                    else
-                        System.out.print(' ');
-                }
-                else {
-                    int startDraw = (width / 2) - (width-(i+1));
-                    int endDraw = (width / 2) + (width-(i+1));
-                    if (j >= startDraw && j <= endDraw)
-                        System.out.print('*');
-                    else
-                        System.out.print(' ');
-                }
+            if(i<n) {
+                int startDraw = (width / 2) - i;
+                int endDraw = (width / 2) + i;
+                drawLine(width, startDraw, endDraw);
+            }
+            else {
+                int startDraw = (width / 2) - (width-(i+1));
+                int endDraw = (width / 2) + (width-(i+1));
+                drawLine(width, startDraw, endDraw);
             }
             System.out.println();
         }
---
+++ src/DiamondExercises/DiamondWithName.java
@@ -0,0 +1,11 @@
+package DiamondExercises;
+
+/**
+ * Created by Tobie Wee
+ * on 6/5/2016.
+ */
+public class DiamondWithName {
+    public static void main (String [] args) {
+
+    }
+}
