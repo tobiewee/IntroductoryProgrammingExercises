db325dfae49f0470bf4c7e30d9e1966a4e7f3ce1
Tobie Wee <tobiewee@gmail.com>
2016-06-05 23:11:32 +0800

    Setup for Prime Factors Exercise

---
+++ src/PrimeFactorsExercise/PrimeFactors.java
@@ -0,0 +1,15 @@
+package PrimeFactorsExercise;
+
+/**
+ * Created by Tobie Wee
+ * on 6/5/2016.
+ */
+public class PrimeFactors {
+    public static void generate(int n){
+        
+    }
+
+    public static void main (String [] args) {
+        generate(30);
+    }
+}
